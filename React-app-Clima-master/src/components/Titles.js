b1b5a209dffc92cbeb359dca001a272824b81e64
Gomi156 <81253325+Gomi156@users.noreply.github.com>
2022-05-24 16:22:44 -0500

    Add files via upload

---
+++ React-app-Clima-master/src/components/Titles.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		
+		<h3 className="title-container__subtitle">Descubrí el clima de cualquier ciudad</h3>
+		<h4 className="title-container__subtitle">Poyecto ll</h4>
+
+	</div>
+);
+
+export default Titles;
